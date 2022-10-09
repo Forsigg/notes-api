29949b8076feb9f243d1eda40d92de6ea8cd9b9a
forsigg <al.berhmiller@gmail.com>
2022-10-09 21:40:18 +0700

    Добавлен тест для регистрации пользователя, изменен сериалайзер для пользователя

--- notes/urls.py
+++ notes/urls.py
@@ -3,8 +3,10 @@ from rest_framework import routers
 from notes.views import NoteViewSet
 
 router = routers.SimpleRouter()
-router.register(r"notes", NoteViewSet, basename="notes")
+router.register(r"notes", NoteViewSet, basename="notes")  # URL: /api/v1/notes
 
+
+# URL: /api/v1/...
 urlpatterns = [
     path("notes/users/<int:user_pk>/", NoteViewSet.as_view({"get": "list_by_user_id"}))
 ]
---
+++ tests/users/tests.py
@@ -0,0 +1,23 @@
+import json
+
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+
+from users.serializers import UserSerializer
+
+
+class UserTests(APITestCase):
+
+    def test_add_user(self):
+        self.assertEqual(0, User.objects.count())
+        resp = self.client.post(
+            '/api/v1/auth/register/',
+            data={'username': "test",
+                  'password': 'test'}
+        )
+        json_resp = json.loads(resp.content)['detail']['data']
+        self.assertEqual(resp.status_code, 201)
+        user = User.objects.get(username='test')
+        serializer = UserSerializer(user)
+        self.assertEqual(json_resp, {'id': serializer.data['id'],
+                                     'username': serializer.data['username']})
