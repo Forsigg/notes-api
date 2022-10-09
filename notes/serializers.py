062376b43655917b23748e39606f21daba367c3d
forsigg <al.berhmiller@gmail.com>
2022-10-09 21:51:16 +0700

    Изменен ридми, добавлены небольшие док стринги

--- notes/serializers.py
+++ notes/serializers.py
@@ -4,12 +4,18 @@ from notes.models import Note, Category
 
 
 class NoteSerializer(serializers.ModelSerializer):
+    """
+    Сериалайзер для модели заметки (Note)
+    """
     class Meta:
         model = Note
         fields = "__all__"
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """
+    Сериалайзер для модели категории (Category)
+    """
     class Meta:
         model = Category
         fields = "__all__"
--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,9 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Сериалайзер для модели пользователя (User)
+    """
     class Meta:
         model = User
         fields = "__all__"
