4acdbe60d736592c8033e2f1e0242c05891d551d
forsigg <al.berhmiller@gmail.com>
2022-10-18 14:31:49 +0600

    Добавлены роуты для CategoryViewSet

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework import routers
-from notes.views import NoteViewSet
+from notes.views import NoteViewSet, CategoryViewSet
 
 router = routers.SimpleRouter()
 router.register(r"notes", NoteViewSet, basename="notes")  # URL: /api/v1/notes
+router.register(r'categories', CategoryViewSet, basename='categories') # URL:
+# /api/v1/categories
 
 
 # URL: /api/v1/...
